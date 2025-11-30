9c730ffd77a6e2bc47efebd2d47716c2167452d2
fish-yummy <sss3110908420@gmail.com>
2025-11-30 21:44:55 +0800

    js分開

--- Js/Main.js
+++ Js/Main.js
@@ -4,8 +4,8 @@ import { initializeNavigation } from './navigation.js';
 // ▼▼▼ 假設您的粒子設定檔名現在是 'particle-config.js' ▼▼▼
 import particleConfig from './particle-config.js'; 
 import { initializeModalMenu } from './modal-menu.js';
-import { img } from './MainPage1Verb.js';
-import * as MainPage1Verb from "./MainPage1Verb.js";
+// import { img } from './MainPage1Verb.js';
+// import * as MainPage1Verb from "./MainPage1Verb.js";
 
 
 
@@ -35,71 +35,6 @@ function onPlayerReady(event) {
 }
 
 
-//class="svgBack" 滑鼠點擊關閉 class="audioPage"
-MainPage1Verb.svgTrigger.addEventListener("click", () => {
-  
-  gsap.to(MainPage1Verb.audioPageTrigger, {
-    scale: 0.9,    
-    opacity: 0,   
-    duration: 0.3,
-    ease: "power2.in",
-    onComplete: () => {
-      MainPage1Verb.audioPageTrigger.style.display = "none";
-      MainPage1Verb.enableScroll();
-      console.log("啟用滾動");
-
-       if(introAudioPlayer && typeof introAudioPlayer.pauseVideo === "function"){
-        introAudioPlayer.pauseVideo();  // 安全呼叫
-      } else {
-        console.warn("introAudioPlayer 尚未準備好");
-      }
-
-      MainPage1Verb.audioPageTrigger.style.transform = "scale(1)"; // 恢復 scale 為1，方便下一次顯示
-    }
-  });
-
- 
-  
-});
-
-//class="audioImage" 點擊開啟 class="audioPage"
-img.addEventListener("click", () => {
-  MainPage1Verb.audioPageTrigger.style.display = "block";
-
- 
-     MainPage1Verb.disableScroll();
-     console.log("禁用滾動");
-  
-
-  gsap.fromTo(
-      MainPage1Verb.audioPageTrigger,
-      { scale: 0.9, opacity: 0 },
-      { scale: 1, opacity: 1, duration: 0.5, ease: "power2.out" }
-  );
-
-});
-
-//class="audioImage" 滑鼠hover
-img.addEventListener("mouseover", () => {
-  img.src = "../Image/MainPage1/播放2.png";   
-  // gsap.fromTo(
-  //     img,
-  //     { opacity: 0 },
-  //     { opacity: 1, duration: 0.5, ease: "power2.out" }
-  // );
-});
-img.addEventListener("mouseout", () => {
-  
-  img.src = "../Image/MainPage1/播放1.png"; 
-  //   gsap.to(img, {  
-  //   opacity: 0,   
-  //   duration: 0.5,
-  //   ease: "power2.in",
-  //   onComplete: () => {
-  //     img.style.opacity = "1";
-  //   }
-  // });
-});
 
 
 
